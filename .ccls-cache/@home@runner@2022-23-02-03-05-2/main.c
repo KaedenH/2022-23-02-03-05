ec679e157a025412dce47f163b9537c8d9e76340
Kaeden Harris <>
2023-01-23 15:17:24 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@2022-23-02-03-05-2/main.c
@@ -0,0 +1,11 @@
+// TODO: write a program that reverses a character arrray
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char s1[] = "racecar";
+  char s2[] = ""
+  size_t len_s1 = strlen(s1);
+  
+  return 0;
+}
